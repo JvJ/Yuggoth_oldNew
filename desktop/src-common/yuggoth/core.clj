2045dbc7f5e021e836a0b9b4b2a4e837e61df281
JvJ <kfjwheeler@gmail.com>
2014-07-28 20:24:18 -0700

    Global caching to spritesheets.

--- desktop/src-common/yuggoth/core.clj
+++ desktop/src-common/yuggoth/core.clj
@@ -1,13 +1,30 @@
 (ns yuggoth.core
   (:require [play-clj.core :refer :all]
-            [play-clj.ui :refer :all]))
+            [play-clj.ui :refer :all]
+            [play-clj.g2d :refer :all]
+            [yuggoth.sprite :refer :all]))
+
+(defonce yuggoth-asset-manager (asset-manager))
+(set-asset-manager! yuggoth-asset-manager)
+
+(declare yuggoth main-screen)
+
+(defn on-show
+  [screen entities]
+  (update! screen :renderer (stage))
+  (let [spritesheet (load-spritesheet "spaceman_sheet_hires.png" :cell-width 64 :cell-height 96)
+        ;_ spritesheet
+        ]
+    [(label "Hello world!" (color :white))
+
+     ;(assoc (spritesheet [0 1]) :x 0 :y 0)
+     (assoc(spritesheet [0 2]) :x 0 :y 0)
+     ]))
 
 (defscreen main-screen
   :on-show
-  (fn [screen entities]
-    (update! screen :renderer (stage))
-    (label "Hello world!" (color :white)))
-  
+  on-show
+
   :on-render
   (fn [screen entities]
     (clear!)
@@ -17,3 +34,6 @@
   :on-create
   (fn [this]
     (set-screen! this main-screen)))
+
+(comment
+  (app! :post-runnable #(set-screen! yuggoth main-screen)))
